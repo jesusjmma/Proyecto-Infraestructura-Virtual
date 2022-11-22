18304e76e7413dfd32005b3e0495d141f3868c06
Daniel Pérez Ruiz <danielperezruiz.10@gmail.com>
2022-11-22 20:35:08 +0100

    Avanzo en la tarea #7, definición de atributos y constructor de clase gastoCompartido.py
    Se ajusta la representación de la clase para modelar adecuadamente la relación entre varios usuarios y un gasto común.

--- src/gastoCompartido.py
+++ src/gastoCompartido.py
@@ -1,5 +1,6 @@
 from usuario import usuario
 from deuda import deuda
+from datetime import datetime as dt
 
 class gastoCompartido:
     '''
@@ -9,8 +10,43 @@ class gastoCompartido:
     Se define con un nombre identificativo del gasto, una lista de usuarios
     participantes, y una lista de deudas
     '''
-    pass
+    
+    #============================================================================
 
+    def __init__usuarios(self, usuarios):
+        '''
+        Copia una lista de usuarios en otra lista
+        (evita duplicado de punteros)
+        Return: copia de usuarios
+        '''
+        listaUsuarios = []
+        for usuario in usuarios:
+            usuarios.append(usuario)
+
+        return listaUsuarios
+    
+    #============================================================================
+
+    def __init__(self,id,nombre,usuarios):
+        '''
+        Constructor de la clase. Se define un numero identificador del gasto
+        compartido, un nombre, una fecha en la que se produjo el gasto compartido,
+        una lista de usuarios y una lista comun de deudas
+        '''
+        self.__id = id
+        self.__nombre = nombre
+        self.__fecha = dt.today()
+        self.__usuarios = self.__init__usuarios(usuarios)
+        self.__deudas = []
+
+    #============================================================================
+
+    def __str__(self):
+        '''
+        Representacion de la clase a formato legible
+        '''
+        pass
+        
 #Test de clase
 if __name__ == "__main__":
     pass
