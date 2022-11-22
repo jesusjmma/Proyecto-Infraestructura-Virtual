78f46c51957e7493575376897a465ff9d3608b2e
Daniel Pérez Ruiz <danielperezruiz.10@gmail.com>
2022-11-22 10:01:55 +0100

    Avanzo en la tarea #8, implementacion de salida a texto de clase deuda.py
    Se sobrecarga el metodo _str_ para que la representacion de la clase en formato string sea entendible y legible. Test OK. Frozen class: No.

--- src/deuda.py
+++ src/deuda.py
@@ -20,9 +20,28 @@ class deuda:
     fecha: dt = field(init=False, repr=True)
 
     def __post_init__(self):
+        '''
+        Tras inicializar la clase, se inicializa la fecha (hoy)
+        '''
         self.fecha = dt.today()
 
-    
+    def __str__(self):
+        '''
+        Representacion de la clase a formato legible
+        '''
+        output = ""
+        output += "=============================================\n"
+        output += "ID Deuda: " + str(self.id) + "\n"
+        output += "=============================================\n"
+        output += "Deber a: " + self.usuarioDeber.nickname + "\n"
+        output += "Paga la deuda: " + self.usuarioDeudor.nickname + "\n"
+        output += "=============================================\n"
+        output += " -- Cantidad: " + str(self.importe) + " €\n"
+        output += " -- Concepto: " + self.concepto + "\n"
+        output += " -- Fecha: " + self.fecha.strftime("%m/%d/%Y") + "\n"
+
+        return output
+        
 
 #Test de clase
 if __name__ == "__main__":
