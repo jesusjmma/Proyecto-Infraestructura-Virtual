8c80bd4f7f604365b916d6581348d44a84f7bcec
Daniel Pérez Ruiz <danielperezruiz.10@gmail.com>
2022-11-22 09:50:09 +0100

    Avanzo en la tarea #8, implementación de timestamp automático en clase deuda.py
    El campo de fecha se inicializa tras la inicializacion de la clase. Todavia falta seguir haciendo pruebas de testeo de la clase

--- src/deuda.py
+++ src/deuda.py
@@ -1,11 +1,11 @@
-from dataclasses import dataclass
-from datetime import datetime
-import usuario
+from dataclasses import dataclass, field
+from datetime import datetime as dt
+from usuario import usuario
 
-@dataclass
+@dataclass(frozen=False)
 class deuda:
     '''
-    Clase que representa la deuda de un usuario frente a otro.
+    Clase que representa la deuda de un usuario a otro.
     '''
     id: int
     #Usuario que debe pagar la deuda
@@ -14,5 +14,23 @@ class deuda:
     usuarioDeber: usuario
 
     importe: float
-    fecha: datetime
     concepto: str
+
+    #Fecha en la que se produce la deuda (generada tras inicializacion)
+    fecha: dt = field(init=False, repr=True)
+
+    def __post_init__(self):
+        self.fecha = dt.today()
+
+    
+
+#Test de clase
+if __name__ == "__main__":
+    u1 = usuario(1,"danielsp","Daniel","Pérez Ruiz")
+    u2 = usuario(2,"pablonl","Pablo","Nieto López")
+
+    d1 = deuda(1,u1,u2,10.5,"test")
+
+    print(d1)
+
+    
