f1f0ecd76035ec06c11af44d71cb8c81bfc8e6b6
Daniel Pérez Ruiz <danielperezruiz.10@gmail.com>
2022-11-22 11:25:36 +0100

    Avanzo en la tarea #7, creacion de clase gastoCompartido.py
    Esta entidad debe ser el corazon (o una de las componentes principales) de la logica de negocio. El objetivo es modelar el tronco de la aplicacion proporcionando la estructura de control necesaria para la prediccion, y actualizacion de datos.

---
+++ src/gastoCompartido.py
@@ -0,0 +1,16 @@
+from usuario import usuario
+from deuda import deuda
+
+class gastoCompartido:
+    '''
+    Entidad que representa a un gasto en el que se ven involucrados una serie de
+    usuarios de la aplicacion, y donde todos tienen una aportacion
+
+    Se define con un nombre identificativo del gasto, una lista de usuarios
+    participantes, y una lista de deudas
+    '''
+    pass
+
+#Test de clase
+if __name__ == "__main__":
+    pass
