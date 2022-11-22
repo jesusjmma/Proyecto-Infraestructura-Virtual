5a83adf1fc3bc1e0aaf69e5962f688413c4bf45b
Daniel Pérez Ruiz <danielperezruiz.10@gmail.com>
2022-11-22 10:31:46 +0100

    Avanzo en la tarea #7, creacion clase deudasUsuario.py
    Entidad necesaria para la lógica de negocio especificada. Permite el seguimiento de todas las deudas de un usuario concreto.

---
+++ src/deudasUsuario.py
@@ -0,0 +1,12 @@
+from usuario import usuario
+from deuda import deuda
+
+class deudasUsuario:
+    '''
+    Entidad que representa las deudas asociadas de un usuario concreto
+    '''
+    pass
+
+#Test de clase
+if __name__ == "__main__":
+    pass
