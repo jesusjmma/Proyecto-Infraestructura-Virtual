879d9d2272e5a24daf59c0d31bfd137fdc8430f0
Daniel Pérez Ruiz <danielperezruiz.10@gmail.com>
2022-11-22 11:29:23 +0100

    Avanzo en la tarea #7, definicion de atributos de instancia de la  clase usuario.py
    Definicion inicial de atributos en función de las conclusiones extraidas del issue.

--- src/usuario.py
+++ src/usuario.py
@@ -6,7 +6,18 @@ class usuario:
     Entidad que representa a un usuario, con sus datos identificativos
     y ademas contiene la informacion de todas las deudas que tiene pendientes
     '''
-    pass
+    
+    def __init__(self, u):
+        '''
+        Constructor de la clase
+        Param u : informacion de un usuario
+        '''
+        #Informacion del usuario
+        self.__usuario = u
+        #Listado de deudas pendientes
+        self.__deudas = []
+
+    
 
 #Test de clase
 if __name__ == "__main__":
