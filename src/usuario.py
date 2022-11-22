2f499e91b93c53172a26eb0c02d3f971ca9cb62f
Daniel Pérez Ruiz <danielperezruiz.10@gmail.com>
2022-11-22 11:42:45 +0100

    Avanzo en la tarea #7, definicion de metodos de insercion,borrado e impresion de datos de clase usuario.py
    Se permiten asignar y eliminar deudas a un usuario, además de recuperar la información de las deuads, siguiendo con la motivación del issue. Test NO.

--- src/usuario.py
+++ src/usuario.py
@@ -17,7 +17,48 @@ class usuario:
         #Listado de deudas pendientes
         self.__deudas = []
 
+    #=================================================================
     
+    def insertarDeuda(self, deuda):
+        '''
+        Asigna una nueva deuda al usuario
+        Param deuda: nueva deuda
+        '''
+        pass
+
+    def eliminarDeuda(self, deuda):
+        '''
+        Elimina una deuda al usuario, a traves del objeto
+        Param deuda: deuda a eliminar
+        '''
+        pass
+
+    def eliminarDeudaPorId(self,idDeuda):
+        '''
+        Elimina una deuda al usuario, a traves de su id
+        Param idDeuda: id de deuda a Eliminar
+        '''
+        pass
+
+    #=================================================================
+    
+    def printListaDeudas(self):
+        '''
+        Imprime la lista de deudas asociadas al usuario
+        '''
+        pass
+
+    def printCantidadTotalDeber(self):
+        '''
+        Imprime por pantalla la cantidad total a deber
+        '''
+        pass
+
+    def __str__(self):
+        '''
+        Representacion de la clase a formato legible
+        '''
+        pass
 
 #Test de clase
 if __name__ == "__main__":
