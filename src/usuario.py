82d1fd7a4658aa4130e0ff92023b8f73ffeb4a90
Daniel Pérez Ruiz <danielperezruiz.10@gmail.com>
2022-11-26 11:49:53 +0100

    Modifico usuario.py eliminando métodos de representación de clase
    Dejaré únicamente la representación de la estructura

--- src/usuario.py
+++ src/usuario.py
@@ -17,39 +17,6 @@ class usuario:
         if(deuda.nickUsuarioQuePaga() == self.obtenerNickUsuario()):
             self.__deudas.append(deuda)
 
-    #=================================================================
-    
-    def obtenerListaDeudasString(self):
-        output = ""
-        output += "=========================================================\n"
-        output += "Lista de deudas:\n"
-        output += "=========================================================\n"
-        if self.__deudas:
-            for deuda in self.__deudas:
-                output += "-> Fecha: " + deuda.obtenerFechaDeuda() + " || "
-                output += "Importe: " + str(deuda.importe) + " || "
-                output += "Concepto: " + deuda.concepto + " || "
-                output += "Pagar A: " + deuda.nickUsuarioDeber() + "\n"
-
-            output += "\n"
-        else:
-            output += "-> No hay deudas pendientes de pago.\n"
-
-        return output
-
-    #=================================================================
-    
-    def printListaDeudas(self):
-        print(self.obtenerListaDeudasString())
-
-    def __str__(self):
-        output = ""
-        output += self.__usuario.__str__() + "\n"
-        output += self.obtenerListaDeudasString()
-
-        return output
-
-
 #Test de clase
 if __name__ == "__main__":
     pass
