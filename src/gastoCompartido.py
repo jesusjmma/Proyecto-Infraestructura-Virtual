6b603bb31e7fd6bc5c26c9891e0071c492dd02de
Daniel Pérez Ruiz <danielperezruiz.10@gmail.com>
2022-11-26 12:07:16 +0100

    Completo gastoCompartido.py modificando constructor #7
    Se permite asociar directamente los gastos y las deudas.

--- src/gastoCompartido.py
+++ src/gastoCompartido.py
@@ -6,39 +6,19 @@ from gasto import gasto
 class gastoCompartido:
     '''
     Entidad que representa a un gasto en el que se ven involucrados una serie de
-    usuarios de la aplicacion, y donde todos tienen una aportacion
+    usuarios de la aplicacion, y donde todos tienen una aportacion, ya sea
+    mediante un gasto, o una deuda
 
     Se define con un nombre identificativo del gasto, una lista de usuarios
-    participantes, y una lista de deudas
+    participantes, una lista de gastos cometidos, y una lista de deudas
     '''
-    
-    #============================================================================
 
-    def __obtenerNicksUsuarios(self):
-        '''
-        Para la lista de usuarios que participan en el gasto compartido,
-        se genera una lista con los nicks de los usuarios
-        Return: lista de nicks de usuario participantes
-        '''
-        nicks = [usuario.obtenerInfoUsuario().nickname.lower() for usuario in self.__usuarios]
-
-        return nicks
-    
-    
-    #============================================================================
-
-    def __init__(self,id,nombre,usuarios=[]):
-        '''
-        Constructor de la clase. Se define un numero identificador del gasto
-        compartido, un nombre, una fecha en la que se produjo el gasto compartido,
-        una lista de usuarios, una lista comun de gastos realizados por los usuarios,
-        y una lista de deudas entre usuarios
-        '''
+    def __init__(self,id,nombre,usuarios=[],gastos=[],deudas=[]):
         self.__id = id
         self.__nombre = nombre
         self.__usuarios = [usuario for usuario in usuarios]
-        self.__gastos = []
-        self.__deudas = []
+        self.__gastos = [gasto for gasto in gastos]
+        self.__deudas = [deuda for deuda in deudas]
         
 #Test de clase
 if __name__ == "__main__":
