31b374b350ce35f90c6630599c2037e04cd48252
Daniel Pérez Ruiz <danielperezruiz.10@gmail.com>
2022-11-26 11:56:47 +0100

    Modifico gasto.py eliminando métodos de representación de clase

--- src/gasto.py
+++ src/gasto.py
@@ -11,21 +11,6 @@ class gasto:
     importe: float
     concepto: str
 
-    def __str__(self):
-        output = ""
-        output += "=============================================\n"
-        output += "Pagado por: " + self.nickUsuarioGasto() + "\n"
-        output += "=============================================\n"
-        output += " -- Importe: " + str(self.importe) + " €\n"
-        output += " -- Concepto: " + self.concepto + "\n"
-
-        return output
-    
-    # ===============================================================
-    
-    def nickUsuarioGasto(self):
-        return self.usuario.nickname
-
 #Test de clase
 if __name__ == "__main__":
     pass
