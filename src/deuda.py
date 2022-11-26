15f2ecd0553d84e033b77d946e244d5599798637
Daniel Pérez Ruiz <danielperezruiz.10@gmail.com>
2022-11-26 11:19:05 +0100

    Elimino atributo id,fecha de clase deuda.py #8
    Atributos no necesarios para objeto valor y para milestone actual.

--- src/deuda.py
+++ src/deuda.py
@@ -1,5 +1,4 @@
-from dataclasses import dataclass, field
-from datetime import datetime as dt
+from dataclasses import dataclass
 from infoUsuario import infoUsuario
 
 @dataclass(frozen=True)
@@ -7,23 +6,11 @@ class deuda:
     '''
     Clase que representa la deuda de un usuario a otro.
     '''
-    id: int
-    #Usuario que recibe la deuda
     usuarioDeber: infoUsuario
-    #Usuario que debe pagar la deuda
     usuarioDeudor: infoUsuario    
 
     importe: float
     concepto: str
-
-    #Fecha en la que se produce la deuda (generada tras inicializacion)
-    fecha: dt = field(init=False, repr=True)
-
-    def __post_init__(self):
-        '''
-        Tras inicializar la clase, se inicializa la fecha (hoy)
-        '''
-        self.fecha = dt.today()
 
     def __str__(self):
         '''
@@ -31,43 +18,22 @@ class deuda:
         '''
         output = ""
         output += "=============================================\n"
-        output += "ID Deuda: " + str(self.id) + "\n"
-        output += "=============================================\n"
         output += "Deber a: " + self.usuarioDeber.nickname + "\n"
         output += "Paga la deuda: " + self.usuarioDeudor.nickname + "\n"
         output += "=============================================\n"
         output += " -- Importe: " + str(self.importe) + " €\n"
         output += " -- Concepto: " + self.concepto + "\n"
-        output += " -- Fecha: " + self.obtenerFechaDeuda() + "\n"
 
         return output
 
     # ===============================================================
 
     def nickUsuarioDeber(self):
-        '''
-        Metodo para obtener el nickname del usuario a quien debe
-        esta deuda
-        Return: nickname de usuario a deber
-        '''
         return self.usuarioDeber.nickname
 
     def nickUsuarioQuePaga(self):
-        '''
-        Metodo para obtener el nickname del usuario que paga
-        esta deuda
-        Return: nickname del usuario que paga
-        '''
         return self.usuarioDeudor.nickname
-    
-    def obtenerFechaDeuda(self):
-        '''
-        Metodo para obtener la fecha de la deuda en formato string
-        Return: fecha en string
-        '''
-        return self.fecha.strftime("%m/%d/%Y")
         
-
 #Test de clase
 if __name__ == "__main__":
     pass
