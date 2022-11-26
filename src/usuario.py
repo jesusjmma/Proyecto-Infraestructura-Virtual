7d3f25ccf9b36a5784a3c2a2916ef78a16b1cedc
Daniel Pérez Ruiz <danielperezruiz.10@gmail.com>
2022-11-26 11:25:32 +0100

    Elimino metodos no correspondientes con milestone actual en clase usuario.py

--- src/usuario.py
+++ src/usuario.py
@@ -8,48 +8,24 @@ class usuario:
     '''
     
     def __init__(self, u):
-        '''
-        Constructor de la clase
-        Param u : informacion de un usuario
-        '''
-        #Informacion del usuario
         self.__usuario = u
-        #Listado de deudas pendientes
         self.__deudas = []
 
     #=================================================================
     
     def insertarDeuda(self, deuda):
-        '''
-        Asigna una nueva deuda al usuario
-        Param deuda: nueva deuda
-        '''
-
-        #Se inserta sii la deuda corresponde con el usuario
         if(deuda.nickUsuarioQuePaga() == self.obtenerNickUsuario()):
             self.__deudas.append(deuda)
 
     def eliminarDeuda(self, deuda):
-        '''
-        Elimina una deuda al usuario, a traves del objeto
-        Param deuda: deuda a eliminar
-        '''
         self.__deudas.remove(deuda)
 
     #=================================================================
         
     def obtenerInfoUsuario(self):
-        '''
-        Recupera la informacion completa asociada a un usuario
-        Return: informacion completa del usuario
-        '''
         return self.__usuario
     
     def obtenerListaDeudasString(self):
-        '''
-        Obtiene en formato string la lista de deudas del usuario
-        Return: String con la lista de deudas de usuario
-        '''
         output = ""
         output += "=========================================================\n"
         output += "Lista de deudas:\n"
@@ -66,43 +42,16 @@ class usuario:
             output += "-> No hay deudas pendientes de pago.\n"
 
         return output
-    
-    def obtenerCantidadTotalDeberString(self):
-        '''
-        Calcula el total de dinero que debe el usuario (sin distincion de
-        a quien debe pagar)
-        Return suma de dinero de todas las deudas (en String)
-        '''
-        total = 0.0
-
-        if self.__deudas:
-            for deuda in self.__deudas:
-                total += deuda.importe
-
-        return str(total) + " €"
 
     #=================================================================
     
     def printListaDeudas(self):
-        '''
-        Imprime la lista de deudas asociadas al usuario
-        '''
         print(self.obtenerListaDeudasString())
 
-    def printCantidadTotalDeber(self):
-        '''
-        Imprime por pantalla la cantidad total a deber
-        '''
-        print("-> Dinero total a deber: " + self.obtenerCantidadTotalDeberString())
-
     def __str__(self):
-        '''
-        Representacion de la clase a formato legible
-        '''
         output = ""
         output += self.__usuario.__str__() + "\n"
         output += self.obtenerListaDeudasString()
-        output += "-> Dinero total a deber: " + self.obtenerCantidadTotalDeberString()
 
         return output
 
