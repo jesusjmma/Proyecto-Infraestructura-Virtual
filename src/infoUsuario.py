990cf7425ba246a6bc30ee28424478c17faec262
Daniel Pérez Ruiz <danielperezruiz.10@gmail.com>
2022-11-26 11:13:13 +0100

    Elimino atributo id de clase infoUsuario.py #7
    Atributo no necesario en un objeto valor.

--- src/infoUsuario.py
+++ src/infoUsuario.py
@@ -5,19 +5,12 @@ class infoUsuario:
     '''
     Clase que representa a un usuario. Contiene todos los datos de identificación del usuario.
     '''
-    id: int
     nickname: str
     nombre: str
     apellidos: str
 
     def __str__(self):
-        '''
-        Representacion de la clase a formato legible
-        '''
         output = ""
-        output += "=============================================\n"
-        output += "ID Usuario: " + str(self.id) + "\n"
-        output += "=============================================\n"
         output += " -- Nickname: " + self.nickname + "\n"
         output += " -- Nombre: " + self.nombre + "\n"
         output += " -- Apellidos: " + self.apellidos + "\n"
