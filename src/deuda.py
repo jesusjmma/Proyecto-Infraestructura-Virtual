10cb47771c154b6d48a39edc158e107cae43a068
Daniel Pérez Ruiz <danielperezruiz.10@gmail.com>
2022-11-26 11:59:23 +0100

    Modifico deuda.py eliminando métodos de representación de clase

--- src/deuda.py
+++ src/deuda.py
@@ -11,28 +11,6 @@ class deuda:
 
     importe: float
     concepto: str
-
-    def __str__(self):
-        '''
-        Representacion de la clase a formato legible
-        '''
-        output = ""
-        output += "=============================================\n"
-        output += "Deber a: " + self.usuarioDeber.nickname + "\n"
-        output += "Paga la deuda: " + self.usuarioDeudor.nickname + "\n"
-        output += "=============================================\n"
-        output += " -- Importe: " + str(self.importe) + " €\n"
-        output += " -- Concepto: " + self.concepto + "\n"
-
-        return output
-
-    # ===============================================================
-
-    def nickUsuarioDeber(self):
-        return self.usuarioDeber.nickname
-
-    def nickUsuarioQuePaga(self):
-        return self.usuarioDeudor.nickname
         
 #Test de clase
 if __name__ == "__main__":
