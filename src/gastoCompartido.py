726d3533738c00f067e21f8b43cb6ffda1954fe7
Daniel Pérez Ruiz <danielperezruiz.10@gmail.com>
2022-11-28 15:52:17 +0100

    Revert "Empiezo desde 0 el objetivo, closes #7"
    This reverts commit 676daea89976904f7e0c356c2e1001592b0c7182.

---
+++ src/gastoCompartido.py
@@ -0,0 +1,21 @@
+from usuario import usuario
+from infoUsuario import infoUsuario
+from deuda import deuda
+from gasto import gasto
+
+class gastoCompartido:
+    '''
+    Entidad que representa a un gasto en el que se ven involucrados una serie de
+    usuarios de la aplicacion, y donde todos tienen una aportacion, ya sea
+    mediante un gasto, o una deuda
+
+    Se define con un nombre identificativo del gasto, una lista de usuarios
+    participantes, una lista de gastos cometidos, y una lista de deudas
+    '''
+
+    def __init__(self,id,nombre,usuarios=[],gastos=[],deudas=[]):
+        self.__id = id
+        self.__nombre = nombre
+        self.__usuarios = [usuario for usuario in usuarios]
+        self.__gastos = [gasto for gasto in gastos]
+        self.__deudas = [deuda for deuda in deudas]
