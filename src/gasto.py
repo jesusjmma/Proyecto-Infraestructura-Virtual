39fe0287a04b491915dda1acd23f624c111d1f81
Daniel Pérez Ruiz <danielperezruiz.10@gmail.com>
2022-11-28 22:15:29 +0100

    closes #11, docstring

--- src/gasto.py
+++ src/gasto.py
@@ -2,6 +2,12 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class gasto:
+    '''
+    Objeto valor que representa un gasto iniciado por un usuario, y en
+    el que participan un conjunto de usuarios, especificando el concepto
+    y el importe del mismo
+    '''
+
     nickPagador: str
     concepto: str
     importe: float
