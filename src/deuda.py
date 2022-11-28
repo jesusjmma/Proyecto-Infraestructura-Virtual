552fce5386dac7e23eba9f4b5c16639f1812d0ad
Daniel Pérez Ruiz <danielperezruiz.10@gmail.com>
2022-11-28 17:57:46 +0100

    Creo fichero deuda.py #12

---
+++ src/deuda.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+
+@dataclass
+class deuda:
+    '''
+    Objeto que representa la deuda que tiene un usuario frente a otro.
+    Consiste de un par de identificadores de usuario (quien recibe y
+    quien paga), además del concepto, importe, y un atributo que comprueba
+    si se ha pagado la deuda o no
+    '''
+    pass
