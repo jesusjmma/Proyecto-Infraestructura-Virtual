74bc5fc0565faa3908549fa3b73caf1b622be610
Daniel Pérez Ruiz <danielperezruiz.10@gmail.com>
2022-11-28 22:00:30 +0100

    :sparkles: feat(usuario.py): resuelve el problema de #13
    Se implementa como entidad, ya que su contenido puede ir variando en función de los gastos pendientes de pago

---
+++ src/usuario.py
@@ -0,0 +1,14 @@
+from gasto import gasto
+
+class usuario:
+
+    def __init__(self,nickname,gastos=[]):
+        self.__nickname = nickname
+        self.__gastos = [gasto for gasto in gastos if self.__nickname in gasto.nicksParticipantes]
+
+    
+    def getNickname(self):
+        return self.__nickname
+    
+    def getGastos(self):
+        return self.__gastos.copy()
