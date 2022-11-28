deba749d200f7fa6bce4d0203b60b37e0e91627f
Daniel Pérez Ruiz <danielperezruiz.10@gmail.com>
2022-11-28 22:15:47 +0100

    closes #13, docstring

--- src/usuario.py
+++ src/usuario.py
@@ -1,6 +1,10 @@
 from gasto import gasto
 
 class usuario:
+    '''
+    Entidad que representa a un usuario, a través de un identificador
+    unívoco mediante nickname y un conjunto de gastos pendientes de pago
+    '''
 
     def __init__(self,nickname,gastos=[]):
         self.__nickname = nickname.lower()
